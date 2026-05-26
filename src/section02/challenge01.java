5766b57920203c05f681fcc08ef253eb26bfef80
fernanda-schmidt <fefe_sac@icloud.com>
2026-05-26 15:54:24 -0300

    challenge 01 - if statements

---
+++ src/section02/challenge01.java
@@ -0,0 +1,19 @@
+package section02;
+
+public class challenge01 {
+    static void main() {
+        double num1 = 20.00;
+        double num2 = 80.00;
+        double step3 = (num1 + num2) * 100;
+        double step4 = step3 % 40;
+
+        boolean isNoRemainder = (step4 == 0) ? true : false;
+
+        System.out.println(isNoRemainder);
+
+        if (!isNoRemainder) {
+            System.out.println("Got some remainder");
+        }
+    }
+
+}
