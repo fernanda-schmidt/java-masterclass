0090babc6e41ddcb7708d84e0070ca1d52915ded
fernanda-schmidt <fefe_sac@icloud.com>
2026-06-02 14:11:01 -0300

    Challenge - Playing Cat

---
+++ src/section03/PlayingCat.java
@@ -0,0 +1,17 @@
+package section03;
+
+public class PlayingCat {
+    static void main() {
+        System.out.println(isCatPlaying(true, 10));
+        System.out.println(isCatPlaying(false, 36));
+        System.out.println(isCatPlaying(false, 35));
+
+    }
+
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+        if (summer) {
+            return temperature >= 25 && temperature <= 45;
+        }
+        return temperature >= 25 && temperature <= 35;
+    }
+}
