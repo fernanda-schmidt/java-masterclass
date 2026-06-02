8c4c40e99cfc0427dc45d7a9b694bade7703b503
fernanda-schmidt <fefe_sac@icloud.com>
2026-06-02 12:32:46 -0300

    Method Overload - Video 55 - Challenge

---
+++ src/section02/MethodOverloadChallenge.java
@@ -0,0 +1,17 @@
+package section02;
+
+public class MethodOverloadChallenge {
+    static void main() {
+        System.out.println(convertToCentimeters(68));
+        System.out.println(convertToCentimeters(5, 8));
+    }
+
+    public static double convertToCentimeters(int inches) {
+        return inches * 2.54;
+    }
+
+    public static double convertToCentimeters(int feet, int inches) {
+        int totalInches = (feet * 12) + inches;
+        return convertToCentimeters(totalInches);
+    }
+}
