15cf81b61c93f02d73aaacfa806a2e4d650c0ba1
fernanda-schmidt <fefe_sac@icloud.com>
2026-06-02 14:00:02 -0300

    Challenge - Equality Printer

---
+++ src/section03/IntEqualityPrinter.java
@@ -0,0 +1,23 @@
+package section03;
+
+public class IntEqualityPrinter {
+    static void main() {
+        printEqual(1, 1, 1);
+        printEqual(1, 1, 2);
+        printEqual(-1, -1, -1);
+        printEqual(1, 2, 3);
+        printEqual(1, 2, 1);
+    }
+
+    public static void printEqual(int x, int y, int z) {
+        if (x < 0 || y < 0 || z < 0) {
+            System.out.println("Invalid Value");
+        } else if (x == y && y == z && x == z) {
+            System.out.println("All numbers are equal");
+        } else if (x != y && y != z && x != z) {
+            System.out.println("All numbers are different");
+        } else {
+            System.out.println("Neither all are equal or different");
+        }
+    }
+}
