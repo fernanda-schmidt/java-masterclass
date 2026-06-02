98fc354b43d021ebedd702ca2169b63479c8d444
fernanda-schmidt <fefe_sac@icloud.com>
2026-06-02 11:50:42 -0300

    Method Overload - Video 54

---
+++ src/section02/MethodOverload.java
@@ -0,0 +1,16 @@
+package section02;
+
+public class MethodOverload {
+    static void main() {
+        calculateScore("Tim", 250);
+        calculateScore(250);
+    }
+
+    public static void calculateScore(String playerName, int playerScore) {
+        System.out.println("Player: " + playerName + "\nScore: " + playerScore);
+    }
+
+    public static void calculateScore(int playerScore) {
+        System.out.println("Score: " + playerScore);
+    }
+}
