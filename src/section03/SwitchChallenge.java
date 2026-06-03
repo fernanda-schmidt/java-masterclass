6f0a396c99dd550530171b552fcf2ee4fede09dc
fernanda-schmidt <fefe_sac@icloud.com>
2026-06-03 11:27:25 -0300

    Challenge 01 - Switch - Video 61

---
+++ src/section03/SwitchChallenge.java
@@ -0,0 +1,18 @@
+package section03;
+
+public class SwitchChallenge {
+    static void main() {
+        System.out.println(toNATO('a'));
+    }
+
+    public static String toNATO(char letter) {
+        return switch (letter) {
+            case 'a' -> "Alpha";
+            case 'b' -> "Baker";
+            case 'c' -> "Charlie";
+            case 'd' -> "Dog";
+            case 'e' -> "Easy";
+            default -> "Letter not found";
+        };
+    }
+}
