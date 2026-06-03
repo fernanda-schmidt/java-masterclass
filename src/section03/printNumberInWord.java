f11893cc6d99d94b9668d2ec2a19e96fefb9ad9f
fernanda-schmidt <fefe_sac@icloud.com>
2026-06-03 12:04:52 -0300

    Coding Exercise 01 - printNumberInWord

---
+++ src/section03/printNumberInWord.java
@@ -0,0 +1,34 @@
+package section03;
+
+public class printNumberInWord {
+    static void main() {
+        printNumberInWord(0);
+        printNumberInWord(1);
+        printNumberInWord(2);
+        printNumberInWord(3);
+        printNumberInWord(4);
+        printNumberInWord(5);
+        printNumberInWord(6);
+        printNumberInWord(7);
+        printNumberInWord(8);
+        printNumberInWord(9);
+        printNumberInWord(10);
+    }
+
+    public static void printNumberInWord(int number) {
+        String numberSpelled = switch (number) {
+            case 0 -> "Zero";
+            case 1 -> "One";
+            case 2 -> "Two";
+            case 3 -> "Three";
+            case 4 -> "Four";
+            case 5 -> "Five";
+            case 6 -> "Six";
+            case 7 -> "Seven";
+            case 8 -> "Eight";
+            case 9 -> "Nine";
+            default -> "Other";
+        };
+        System.out.println(number + " = " + numberSpelled);
+    }
+}
