afc04d2e7a7d5efb5317ffbb2b7b1c912f0885d0
fernanda-schmidt <fefe_sac@icloud.com>
2026-06-10 15:49:59 -0300

    Coding Exercise 03 - Sum Odd Numbers with For Loop

---
+++ src/section03/CodingExercise03.java
@@ -0,0 +1,35 @@
+package section03;
+
+public class CodingExercise03 {
+    static void main() {
+        System.out.println(isOdd(3));
+        System.out.println(isOdd(4));
+        System.out.println(sumOdd(1, 100));
+        System.out.println(sumOdd(-1, 100));
+        System.out.println(sumOdd(100, 100));
+        System.out.println(sumOdd(13, 13));
+        System.out.println(sumOdd(100, -100));
+        System.out.println(sumOdd(100, -100));
+        System.out.println(sumOdd(100, 1000));
+    }
+
+    public static boolean isOdd(int number) {
+        if (number < 0) {
+            return false;
+        }
+        return (number % 2 != 0);
+    }
+
+    public static int sumOdd(int start, int end) {
+        if ((start > end) || (start < 0 || end < 0)) {
+            return -1;
+        }
+
+        int sum = 0;
+
+        for (int i = start; i <= end; i++) {
+            if (isOdd(i)) sum += i;
+        }
+        return sum;
+    }
+}
