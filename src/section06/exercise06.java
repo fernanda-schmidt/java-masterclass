7089d15123b724b8bdbd5ba70c73f0539ffac399
fernanda-schmidt <fefe_sac@icloud.com>
2026-07-08 15:55:10 -0300

    Exercises 06 06

---
+++ src/section06/exercise06.java
@@ -0,0 +1,33 @@
+package section06;
+/*
+Write a method named printFactors with one parameter of type int named number.
+If number is < 1, the method should print "Invalid Value".
+The method should print all factors of the number. A factor of a number is an integer which divides that number wholly (i.e. without leaving a remainder).
+For example, 3 is a factor of 6 because 3 fully divides 6 without leaving a remainder. In other words 6 / 3 = 2.
+EXAMPLE INPUT/OUTPUT:
+    printFactors(6); → should print 1 2 3 6
+    printFactors(32); → should print 1 2 4 8 16 32
+    printFactors(10); → should print 1 2 5 10
+    printFactors(-1); → should print "Invalid Value" since number is < 1
+HINT: Use a while or for loop.
+ */
+public class exercise06 {
+    static void main() {
+        printFactors(6);
+        printFactors(32);
+        printFactors(10);
+        printFactors(-1);
+    }
+    public static void printFactors(int number){
+        if (number <1){
+            System.out.println("Invalid Value");
+            return;
+        }
+
+        for (int i = 1; i <= number; i++){
+            if (number % i == 0){
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
